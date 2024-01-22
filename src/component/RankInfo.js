8347e533e8d5a6831a2aa14bb1429bb01eae8366
laon2019 <laon5987@nate.com>
2024-01-22 14:06:19 +0900

    rankinfo 수정

--- src/component/RankInfo.js
+++ src/component/RankInfo.js
@@ -2,9 +2,11 @@ import React, { useEffect } from "react";
 import Image from "react-bootstrap/Image";
 
 const RankInfo = ({ rankInfo }) => {
+  console.log(rankInfo)
   return (
     <div style={{ padding: "10px", margin: "10px", display: "flex", alignItems: "center" }}>
-      <div style={{ marginRight: "10px" }}>
+      {rankInfo ? (
+        <div style={{ marginRight: "10px" }}>
         <Image
           src={`/img/${rankInfo.tier}.jpeg`}
           rounded
@@ -14,10 +16,14 @@ const RankInfo = ({ rankInfo }) => {
           }}
         />
       </div>
-      <div style={{ display: "flex", flexDirection: "column", flex: 1, alignItems: "center", margin: "0 10px" }}>
+      ) : (<></>)}
+      {rankInfo ? (
+        <div style={{ display: "flex", flexDirection: "column", flex: 1, alignItems: "center", margin: "0 10px" }}>
         <p>{rankInfo.tier} {rankInfo.rank}</p>
         <p>{rankInfo.leaguePoints}P</p>
       </div>
+      ):(
+      <></>)}
     </div>
   );
 };
