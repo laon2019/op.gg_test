2d2dec86e4a604d4c5a169bbfaa87d01db53a3c3
laon2019 <laon5987@nate.com>
2024-01-23 21:05:06 +0900

    test

--- src/redux/api.js
+++ src/redux/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "https://",
-    // baseURL: "https://kr.api.riotgames.com/lol",
     headers: {"Content-Type": "application/json"}
 })
 
